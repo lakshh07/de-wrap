0e5db7ac090a57141828029faeb0a5e578394c73
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2025-08-01 10:47:30 +0530

    feat(web): implement AppSidebar, NavMain, and SiteHeader components for improved navigation and user experience

---
+++ apps/web/app/components/app-sidebar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import * as React from "react";
+import {
+  IconChartBar,
+  IconInnerShadowTop,
+  IconCurrencyDollar,
+  IconHome,
+} from "@tabler/icons-react";
+
+import { NavMain } from "@/components/nav-main";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const data = {
+  navMain: [
+    {
+      title: "Home",
+      url: "/home",
+      icon: IconHome,
+    },
+    {
+      title: "Payments",
+      url: "/payments",
+      icon: IconCurrencyDollar,
+    },
+    {
+      title: "Investments",
+      url: "/investments",
+      icon: IconChartBar,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">DeWrap</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+    </Sidebar>
+  );
+}
---
+++ apps/web/app/components/nav-main.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { IconCirclePlusFilled, type Icon } from "@tabler/icons-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon?: Icon;
+  }[];
+}) {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent className="flex flex-col gap-2 mt-2">
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center gap-2">
+            <Link href="/invoices/new" className="w-full">
+              <SidebarMenuButton
+                tooltip="Quick Create Invoice"
+                className="bg-primary active:scale-95 transition-all cursor-pointer text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+              >
+                <IconCirclePlusFilled />
+                <span>Create Invoice</span>
+              </SidebarMenuButton>
+            </Link>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <SidebarMenu className="mt-2">
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <Link href={item.url}>
+                <SidebarMenuButton
+                  className="cursor-pointer"
+                  tooltip={item.title}
+                  isActive={pathname.includes(item.url)}
+                >
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
