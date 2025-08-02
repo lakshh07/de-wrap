a12494ffe0849c238f8273a5c34c3657c6b512ea
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2025-08-03 00:51:23 +0530

    feat(contracts): add dotenv dependency for environment variable management; update hardhat config and turbo.json for new inputs

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -20,6 +21,10 @@ const config: HardhatUserConfig = {
     },
     mainnetForked: {
       url: "http://127.0.0.1:8545",
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    polygonForked: {
+      url: "http://127.0.0.1:8546",
       accounts: [process.env.PRIVATE_KEY!],
     },
     sepolia: {
